912bd039ebc1048b72fd1d9d3f7101c362ef3d66
Sadique <mhsadique1307@gmail.com>
2022-07-25 22:41:14 +0600

    homepage done

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,9 +1,46 @@
-import React from "react";
-import { View, Text, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, Pressable, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AntDesign } from "@expo/vector-icons";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { db } from "../../App";
 
 const Home = ({ user, navigation }) => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    // create the query
+    const q = query(collection(db, "notes"), where("uid", "==", user.uid));
+    // create listener to listen to the query that we have just made
+    const notesListenerSubscription = onSnapshot(q, (querySnapshot) => {
+      const list = [];
+      querySnapshot.forEach((doc) => {
+        list.push(doc.data());
+      });
+      setNotes(list);
+    });
+  }, []);
+
+  const renderItem = ({ item }) => {
+    const { title, color, description } = item;
+    return (
+      <Pressable
+        style={{
+          backgroundColor: color,
+          marginBottom: 25,
+          borderRadius: 15,
+          padding: 15,
+        }}
+        key={title}
+      >
+        <Text style={{ color: "white", fontSize: 24 }}>{title}</Text>
+        <Text style={{ color: "white", fontSize: 18, marginTop: 16 }}>
+          {description}
+        </Text>
+      </Pressable>
+    );
+  };
+
   return (
     <SafeAreaView>
       <View
@@ -18,6 +55,14 @@ const Home = ({ user, navigation }) => {
         <Pressable onPress={() => navigation.navigate("Create")}>
           <AntDesign name="pluscircleo" size={24} color="black" />
         </Pressable>
+      </View>
+      <View>
+        <FlatList
+          data={notes}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.title}
+          contentContainerStyle={{ padding: 20 }}
+        />
       </View>
     </SafeAreaView>
   );
